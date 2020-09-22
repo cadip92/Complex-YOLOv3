25b31e4c62f482211d2de385390db12c81dc1f27
cadip92 <cadip92@gmail.com>
2020-09-22 11:09:56 +0200

    Images with bounding boxes are now saved as image files

--- test_detection.py
+++ test_detection.py
@@ -1,10 +1,9 @@
 import numpy as np
-import math
-import os
 import argparse
 import cv2
-import time
-import torch
+import time, json, os, math, sys
+from pprint import pprint
+from easydict import EasyDict as edict
 
 import utils.utils as utils
 from models import *
@@ -86,28 +85,44 @@ def predictions_to_kitti_format(img_detections, calib, img_shape_2d, img_size, R
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_def", type=str, default="config/complex_tiny_yolov3.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="checkpoints/tiny-yolov3_ckpt_epoch-220.pth", help="path to weights file")
-    parser.add_argument("--class_path", type=str, default="data/classes.names", help="path to class label file")
-    parser.add_argument("--conf_thres", type=float, default=0.5, help="object confidence threshold")
-    parser.add_argument("--nms_thres", type=float, default=0.5, help="iou thresshold for non-maximum suppression")
-    parser.add_argument("--img_size", type=int, default=cnf.BEV_WIDTH, help="size of each image dimension")
-    parser.add_argument("--split", type=str, default="valid", help="text file having image lists in dataset")
-    parser.add_argument("--folder", type=str, default="training", help="directory name that you downloaded all dataset")
-    opt = parser.parse_args()
-    print(opt)
 
-    classes = utils.load_classes(opt.class_path)
+    parser.add_argument('--config', default=None, type=str, help='Configuration file')
+    parser = parser.parse_args()
+
+    try:
+        if parser.config is not None:
+            with open(parser.config, 'r') as config_file:
+                config_args_dict = json.load(config_file)
+        else:
+            print("Add a config file using \'--config file_name.json\'", file=sys.stderr)
+            exit(1)
+
+    except FileNotFoundError:
+        print("ERROR: Config file not found: {}".format(parser.config), file=sys.stderr)
+        exit(1)
+    except json.decoder.JSONDecodeError:
+        print("ERROR: Config file is not a proper JSON file!", file=sys.stderr)
+        exit(1)
+
+    FLAGS = edict(config_args_dict)
+
+    if FLAGS.checkpoint_file == "":
+        print("ERROR: Checkpoint file is not specified", file=sys.stderr)
+        exit(1)
+
+    pprint(FLAGS)
+
+    classes = utils.load_classes(FLAGS.class_path)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Set up model
-    model = Darknet(opt.model_def, img_size=opt.img_size).to(device)
+    model = Darknet(FLAGS.model_def, img_size=cnf.BEV_WIDTH).to(device)
     # Load checkpoint weights
-    model.load_state_dict(torch.load(opt.weights_path))
+    model.load_state_dict(torch.load(FLAGS.checkpoint_file))
     # Eval mode
     model.eval()
     
-    dataset = KittiYOLODataset(cnf.root_dir, split=opt.split, mode='TEST', folder=opt.folder, data_aug=False)
+    dataset = KittiYOLODataset(cnf.root_dir, split=FLAGS.split, mode='TEST', folder=FLAGS.folder, data_aug=False)
     data_loader = torch_data.DataLoader(dataset, 1, shuffle=False)
 
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
@@ -121,7 +136,7 @@ if __name__ == "__main__":
         # Get detections 
         with torch.no_grad():
             detections = model(input_imgs)
-            detections = utils.non_max_suppression_rotated_bbox(detections, opt.conf_thres, opt.nms_thres) 
+            detections = utils.non_max_suppression_rotated_bbox(detections, FLAGS.conf_thres, FLAGS.nms_thres) 
         
         end_time = time.time()
         print(f"FPS: {(1.0/(end_time-start_time)):0.2f}")
@@ -145,7 +160,7 @@ if __name__ == "__main__":
                 continue
 
             # Rescale boxes to original image
-            detections = utils.rescale_boxes(detections, opt.img_size, RGB_Map.shape[:2])
+            detections = utils.rescale_boxes(detections, cnf.BEV_WIDTH, RGB_Map.shape[:2])
             for x, y, w, l, im, re, conf, cls_conf, cls_pred in detections:
                 yaw = np.arctan2(im, re)
                 # Draw rotated box
@@ -153,12 +168,13 @@ if __name__ == "__main__":
 
         img2d = cv2.imread(img_paths[0])
         calib = kitti_utils.Calibration(img_paths[0].replace(".png", ".txt").replace("image_2", "calib"))
-        objects_pred = predictions_to_kitti_format(img_detections, calib, img2d.shape, opt.img_size)  
+        objects_pred = predictions_to_kitti_format(img_detections, calib, img2d.shape, cnf.BEV_WIDTH)  
         
         img2d = mview.show_image_with_boxes(img2d, objects_pred, calib, False)
-        
-        cv2.imshow("bev img", RGB_Map)
-        cv2.imshow("img2d", img2d)
+
+        cv2.imwrite("test_%s.jpeg" % index, img2d)
+        #cv2.imshow("bev img", RGB_Map)
+        #cv2.imshow("img2d", img2d)
 
         if cv2.waitKey(0) & 0xFF == 27:
             break
